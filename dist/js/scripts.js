9fbbba140497f56422541ce845e865e8d9959f01
Madkunt <77546176+Madkunt@users.noreply.github.com>
2021-07-21 09:03:01 +1000

    Update scripts.js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
 * Start Bootstrap - Business Casual v7.0.2 (https://startbootstrap.com/theme/business-casual)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/MadKunt/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
 // Highlights current date on contact page
 window.addEventListener('DOMContentLoaded', event => {
